9957b7eb1b5fb06ff33b0220267abb586f5c54d1
kavita <sharma14.kavita@gmail.com>
2018-12-26 21:20:42 +0530

    fsbo crawler

---
+++ fsbo.com/crawler.py
@@ -0,0 +1,62 @@
+import csv
+import json
+from parsel import Selector
+from time import sleep
+from selenium import webdriver
+from lxml import html
+from selenium.webdriver.common.keys import Keys
+import re
+import sys
+import uuid
+import os
+
+#PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+DRIVER_BIN = "/Users/kavitasharma/Downloads/chromedriver"
+driver = webdriver.Chrome(DRIVER_BIN)
+urls=[]
+
+driver.get('https://fsbo.com/listings/search/')
+username = driver.find_element_by_name('state')
+username.send_keys('Alaska')
+sleep(2)
+
+driver.find_element_by_id("advancedSearchForm").submit();
+elems = driver.find_elements_by_xpath("//a[text() = 'View Listing Details']")
+for elem in elems:
+    urls.append(elem.get_attribute("href"))
+while(1):
+    sleep(2)
+    nextpage = driver.find_elements_by_class_name("nextPage")
+    if nextpage:
+        sleep(2)
+        nextpage[0].click()
+        elems = driver.find_elements_by_xpath("//a[text() = 'View Listing Details']")
+        for elem in elems:
+            urls.append(elem.get_attribute("href"))
+    else:
+        break;
+
+for url in urls:
+    sleep(2)
+    driver.get(url)
+    sel = Selector(text=driver.page_source)
+    property_detail = sel.xpath('//*[starts-with(@class, "listing-data")]//tr').extract()
+    for tr in property_detail:
+        property = Selector(text=tr).xpath('//td//text()').extract()
+        print(property)
+    property_address = sel.xpath('//*[starts-with(@class, "address-copy")]//span//text()').extract()
+    print("property_address")
+    print(property_address)
+
+    amentites = sel.xpath('//div[starts-with(@class, "amenities")]//li//text()').extract()
+    print("amentites")
+    print(amentites)
+
+    owner_detail = experience = sel.xpath('//div[@id="sellerModal"]//text()').extract()
+
+    print("owner detail")
+    print(owner_detail)
+
+    exit()
+
+driver.quit()
